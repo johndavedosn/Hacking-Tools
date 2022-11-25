73b65e1d005c49f4f866a44772fa6d0b2b7b2f5c
johndavedosn <112868391+johndavedosn@users.noreply.github.com>
2022-11-25 17:36:55 +0100

    Update MyClient.py

--- MyClient.py
+++ MyClient.py
@@ -2,7 +2,7 @@ import socket
 
 
 def client_program():
-    host = "troissou"  # as both code is running on same pc
+    host = socket.gethostname()  # as both code is running on same pc
     port = 5000  # socket server port number
     client_socket = socket.socket()  # instantiate
     client_socket.connect((host, port))  # connect to the server
