f3cfa6bebd8c57afeeb6807128fe0b94e850de9c
johndavedosn <112868391+johndavedosn@users.noreply.github.com>
2022-12-08 19:05:40 +0100

    Delete DeviceInfoGetter.py

--- DeviceInfoGetter.py
+++
@@ -1,9 +0,0 @@
-import socket
-import time
-import sys
-hostname=socket.gethostname()
-IPAddr=socket.gethostbyname(hostname)
-print("Your Device Name is : " + hostname)
-print("Your Device IP Address is: " + IPAddr)
-print("Your Device Platform is : " + sys.platform) 
-time.sleep(5)
